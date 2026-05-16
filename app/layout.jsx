f57ab9bb5a2c795eeacf14ebafbc6177679837ff
Mohammed Awaiz <awaiz.web@gmail.com>
2026-05-17 01:14:28 +0530

    V17

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import ConditionalShell from "./components/ConditionalShell";
 import { CartProvider } from "./context/CartContext";
-import Script from "next/script"; 
+import Script from "next/script";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -140,23 +140,43 @@ export default function RootLayout({ children }) {
           content="MRBYKHpOA65wMeP5_F7ZjyPSpcijC_-SxoyV_VgfRkM"
         />
 
-        <!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2884133501797938');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=2884133501797938&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+    !function(f,b,e,v,n,t,s)
+    {
+      if(f.fbq)return;
+      n=f.fbq=function(){
+        n.callMethod ?
+        n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+      };
+      if(!f._fbq)f._fbq=n;
+      n.push=n;
+      n.loaded=!0;
+      n.version='2.0';
+      n.queue=[];
+      t=b.createElement(e);
+      t.async=!0;
+      t.src=v;
+      s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s);
+    }(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '2884133501797938');
+    fbq('track', 'PageView');
+  `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2884133501797938&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
 
       <body
